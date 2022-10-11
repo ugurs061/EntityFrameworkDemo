1bf434dc9b539608a33eaa6a12aec8608926c66b
Uğur Sağlam <ugur.saglam@arksoft.com.tr>
2022-10-11 17:40:13 +0300

    11/10/2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
