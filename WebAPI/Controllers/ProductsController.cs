84567e341ce39b26b3fed49756fc0437781f3d92
Uğur Sağlam <ugur.saglam@arksoft.com.tr>
2022-11-22 17:11:23 +0300

    11.22.2022

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]// Attribute
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
