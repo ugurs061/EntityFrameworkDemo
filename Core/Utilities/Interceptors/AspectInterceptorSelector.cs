d01c4ac415c7500f0d96ec3ffd0f03608984a2f1
Ugur <rextsts@hotmail.com>
2023-02-12 17:35:51 +0300

    aop

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 namespace Core.Utilities.Interceptors
 { 
-
+    // Attributelerin okunması 
     public class AspectInterceptorSelector : IInterceptorSelector
     {
         public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
